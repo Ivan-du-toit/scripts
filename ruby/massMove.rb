c53e0d46895cec1aa52bf4e78c761c0299621165
Ivan du Toit <anilihst@gmail.com>
2014-07-01 19:41:26 +0000

    Added CLI option parser and cleandup some of the code

--- ruby/massMove.rb
+++ ruby/massMove.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 require 'fileutils'
+require 'optparse'
+
 
 config_file = 'config.yaml'
 
@@ -14,31 +16,42 @@ default_config = {
         }, {
             'source'  => '.',
             'dest'    => 'deep/file/path',
-            'match'   => '[0-9]*',
+            'match'   => '([0-9]*)',
+			'replace' => '\1',
             'action'  => 'copy'
         }
     ]
 }
+
+# TODO: Make the config file location configurable by passing it in as a optional paramater.
+# TODO: Add param to generate the source DIR list from a directory structure.
+
+options = {}
+verbose = false
+OptionParser.new do |opts|
+    opts.banner = "Usage: dogPile.rb [options]"
+
+    opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+        verbose = v
+    end
+
+    opts.on("--config [FILE]", String, "Config file location") do |c|
+        config_file = c
+    end
+
+    opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+    end
+end.parse!
+
+sources = ["."]
 
 unless File.exists?(config_file)
     YAML.dump(default_config)
     File.open(config_file, 'w') {|f| f.write default_config.to_yaml }
     puts "No config file found, generating example config to config.yaml"
     exit
-end
-
-# TODO: Make the config file location configurable by passing it in as a optional paramater.
-# TODO: Add a verbose flag and suppress the output if it is missing.
-# TODO: Add param to generate the source DIR list from a directory structure.
-
-verbose = false
-
-if ARGV.length == 0
-    sources.push(".")
-else
-    ARGV.each do |arg|
-        sources.push(arg)
-    end
 end
 
 config = YAML.load_file(config_file)
@@ -54,11 +67,11 @@ for rule in config["rules"] do
 end
 
 matches = 0
-sources = []
 
 sources.each do |sourceDir|
     rules.each do | rule |
-        unless Dir.exists?(sourceDir + "/" + rule["source"])
+        #unless Dir.exists?(sourceDir + "/" + rule["source"])
+        unless File.exists?(sourceDir + "/" + rule["source"])
             puts rule
             raise "Source folder does not exist for: " + sourceDir + "/" + rule["source"]
         end
@@ -70,28 +83,31 @@ sources.each do |sourceDir|
 
                 matches = matches +1
 
-                unless Dir.exists?(sourceDir + "/" + rule["dest"]) 
+                unless File.exists?(sourceDir + "/" + rule["dest"])
+                #unless Dir.exists?(sourceDir + "/" + rule["dest"])
                     if verbose
                         puts "Creating dir: " + rule["dest"]
                     end
                     Dir.mkdir(sourceDir + "/" + rule["dest"])
                 end
 
+                fullSourcePath = sourceDir + "/" + rule["source"] + "/" + file_name
+                fullDestPath = sourceDir + "/" + rule["dest"] + "/" + new_file_name
                 if rule["action"] == "move"
                     if verbose
-                        puts "Moving " + sourceDir + "/" + rule["source"] + "/" + file_name + " to " + sourceDir + "/" + rule["dest"] + "/" + new_file_name
+                        puts "Moving " + fullSourcePath + " to " + fullDestPath
                     end
-                    File.rename(sourceDir + "/" + rule["source"] + "/" + file_name, sourceDir + "/" + rule["dest"] + "/" + new_file_name)
+                    File.rename(fullSourcePath, fullDestPath)
                 elsif rule["action"] == "copy"
                     if  verbose
-                        puts "Copying " + sourceDir + "/" + rule["source"] + "/" + file_name + " to " + sourceDir + "/" + rule["dest"] + "/" + new_file_name
+                        puts "Copying " + fullSourcePath + " to " + fullDestPath
                     end
-                    FileUtils.cp(sourceDir + "/" + rule["source"] + "/" + file_name, sourceDir + "/" + rule["dest"] + "/" + new_file_name)
+                    FileUtils.cp(fullSourcePath, fullDestPath)
                 else
                     warn "Invalid action on rule"
                 end
 
-            end 
+            end
         end
     end
 end
@@ -100,4 +116,3 @@ if verbose
     puts "#{matches} files matches"
     puts "Renaming complete!"
 end
-
